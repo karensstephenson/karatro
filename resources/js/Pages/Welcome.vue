4d61bcc2ce60018ea13da8b5a977eb97a2393eb3
James <electionnightspecial@gmail.com>
2024-04-06 12:30:20 +0100

    Remove code + add import

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { defineProps, ref, computed } from 'vue';
 import { Head, Link } from '@inertiajs/vue3';
+import cards from '@/global/cards';
 
 defineProps<{
     canLogin?: boolean;
@@ -23,40 +25,12 @@ function handleImageError() {
             id="background"
             class="absolute -left-20 top-0 max-w-[877px]"
             src="https://laravel.com/assets/img/welcome/background.svg"
+            alt="Background"
         />
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <!-- <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                        <Link
-                            v-if="$page.props.auth.user"
-                            :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Dashboard
-                        </Link>
-
-                        <template v-else>
-                            <Link
-                                :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
-                                v-if="canRegister"
-                                :href="route('register')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Register
-                            </Link>
-                        </template>
-                    </nav>
-                </header> -->
-
                 <main class="mt-6 flex items-center justify-center">
                     <img src="cards/clubs_2.png" alt="clubs_2" class="w-1/4 h-1/4" />
                 </main>
