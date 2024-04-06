4f291676bbe06918495a17c8cdce8b0a91c5df3a
James <electionnightspecial@gmail.com>
2024-04-06 13:47:35 +0100

    4: import cards + add random button (#8)
    * Remove code + add import
    
    * Create cards.ts
    
    * Add random card button

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import { defineProps, ref, computed } from "vue";
+import { Head, Link } from "@inertiajs/vue3";
+import cards from "@/global/cards";
 
 defineProps<{
     canLogin?: boolean;
@@ -9,11 +11,13 @@ defineProps<{
 }>();
 
 function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
+    document.getElementById("screenshot-container")?.classList.add("!hidden");
+    document.getElementById("docs-card")?.classList.add("!row-span-1");
+    document.getElementById("docs-card-content")?.classList.add("!flex-row");
+    document.getElementById("background")?.classList.add("!hidden");
 }
+
+const randomCard = ref("clubs_2");
 </script>
 
 <template>
@@ -23,42 +27,25 @@ function handleImageError() {
             id="background"
             class="absolute -left-20 top-0 max-w-[877px]"
             src="https://laravel.com/assets/img/welcome/background.svg"
+            alt="Background"
         />
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <!-- <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                    <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
-                        <Link
-                            v-if="$page.props.auth.user"
-                            :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                        >
-                            Dashboard
-                        </Link>
-
-                        <template v-else>
-                            <Link
-                                :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
-                                v-if="canRegister"
-                                :href="route('register')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Register
-                            </Link>
-                        </template>
-                    </nav>
-                </header> -->
-
                 <main class="mt-6 flex items-center justify-center">
-                    <img src="cards/clubs_2.png" alt="clubs_2" class="w-1/4 h-1/4" />
+                    <div class="flex flex-col items-center">
+                        <img
+                            :src="`cards/${randomCard}.png`"
+                            :alt="randomCard"
+                        />
+
+                        <button
+                            class="mt-6 px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
+                        >
+                            New Random Card
+                        </button>
+                    </div>
                 </main>
             </div>
         </div>
