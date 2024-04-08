ac05792e2636adf28e40668f6697912ec38fe0eb
James <electionnightspecial@gmail.com>
2024-04-08 20:38:34 +0100

    Replace background with casino-table green (#30)

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -72,13 +72,7 @@ const showScore = () => {
 
 <template>
     <Head title="Welcome" />
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <img
-            id="background"
-            class="absolute -left-20 top-0 max-w-[877px]"
-            src="https://laravel.com/assets/img/welcome/background.svg"
-            alt="Background"
-        />
+    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 bg-[#00796B] dark:bg-[#00796B]">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
