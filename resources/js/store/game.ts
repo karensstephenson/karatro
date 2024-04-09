2b86b6f23fc0e009311d92cd5905e7a333ed2212
James <electionnightspecial@gmail.com>
2024-04-09 16:46:29 +0100

    Add pinia & build store (#43)

---
+++ resources/js/store/game.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia'
+
+export const useGameStore = defineStore({
+    id: 'game',
+    state: () => ({
+        cards: [] as Card[],
+        hand: [] as Card[],
+        discards: [] as Card[],
+        playedHands: [],
+        totalPoints: 0,
+    }),
+    getters: {
+        remainingDeck(): Card[] {
+            return this.cards.filter(card => !this.hand.includes(card) && !this.discards.includes(card))
+        }
+    },
+})
