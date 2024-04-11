e94f14c3a92b956e5ddb4f2a8039dbf030f9a545
karensstephenson <135253615+karensstephenson@users.noreply.github.com>
2024-04-11 21:06:57 +0100

    added getHandName function (#57)
    * added getHandName function
    
    * made PR changes

--- resources/js/composables/__tests__/hand-calculator.spec.js
+++ resources/js/composables/__tests__/hand-calculator.spec.js
@@ -6,11 +6,11 @@ const { getHandName } = useHandCalculator();
 describe('Hand calculator test suite', () => {
   it('should return High Card when the only applicable card is that', () => {
         const highCardHand = [
-            { value: 2, suit: 'hearts' },
-            { value: 3, suit: 'diamonds' },
-            { value: 4, suit: 'hearts' },
-            { value: 5, suit: 'clubs' },
-            { value: 7, suit: 'spaces' },
+            { value: 2, suit: 'hearts', rank: 2 },
+            { value: 3, suit: 'diamonds', rank: 3 },
+            { value: 4, suit: 'hearts', rank: 4 },
+            { value: 5, suit: 'clubs', rank: 5 },
+            { value: 7, suit: 'spaces', rank: 7 },
         ];
 
         const result = getHandName(highCardHand);
@@ -19,11 +19,11 @@ describe('Hand calculator test suite', () => {
 
     it('should return Pair when the only applicable card is that', () => {
             const pairHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'diamonds' },
-                { value: 4, suit: 'hearts' },
-                { value: 5, suit: 'clubs' },
-                { value: 7, suit: 'spaces' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'diamonds', rank: 2 },
+                { value: 4, suit: 'hearts', rank: 4 },
+                { value: 5, suit: 'clubs', rank: 5 },
+                { value: 7, suit: 'spaces', rank: 7 },
             ];
     
             const result = getHandName(pairHand);
@@ -32,10 +32,10 @@ describe('Hand calculator test suite', () => {
 
     it('should return Two Pair when the only applicable card is that', () => {
             const twoPairHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'diamonds' },
-                { value: 4, suit: 'hearts' },
-                { value: 4, suit: 'clubs' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'diamonds', rank: 2 },
+                { value: 4, suit: 'hearts', rank: 4 },
+                { value: 4, suit: 'clubs', rank: 4 },
             ];
     
             const result = getHandName(twoPairHand);
@@ -44,9 +44,9 @@ describe('Hand calculator test suite', () => {
 
     it('should return Three of a Kind when the only applicable card is that', () => {
             const threeOfAKindHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'diamonds' },
-                { value: 2, suit: 'hearts' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'diamonds', rank: 2 },
+                { value: 2, suit: 'hearts', rank: 2 },
             ];
     
             const result = getHandName(threeOfAKindHand);
@@ -55,11 +55,11 @@ describe('Hand calculator test suite', () => {
 
     it('should return Straight when the only applicable card is that', () => {
             const straightHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 3, suit: 'diamonds' },
-                { value: 4, suit: 'hearts' },
-                { value: 5, suit: 'clubs' },
-                { value: 6, suit: 'spaces' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 3, suit: 'diamonds', rank: 3 },
+                { value: 4, suit: 'hearts', rank: 4 },
+                { value: 5, suit: 'clubs', rank: 5 },
+                { value: 6, suit: 'spades', rank: 6 },
             ];
     
             const result = getHandName(straightHand);
@@ -68,10 +68,10 @@ describe('Hand calculator test suite', () => {
 
     it('should return Flush when the only applicable card is that', () => {
             const flushHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 4, suit: 'hearts' },
-                { value: 6, suit: 'hearts' },
-                { value: 8, suit: 'hearts' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 4, suit: 'hearts', rank: 4 },
+                { value: 6, suit: 'hearts', rank: 6 },
+                { value: 8, suit: 'hearts', rank: 8 },
                 { value: 10, suit: 'hearts', rank: 'J' },
             ];
     
@@ -81,11 +81,11 @@ describe('Hand calculator test suite', () => {
 
     it('should return Full House when the only applicable card is that', () => {
             const fullHouseHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'diamonds' },
-                { value: 2, suit: 'hearts' },
-                { value: 4, suit: 'clubs' },
-                { value: 4, suit: 'spaces' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'diamonds', rank: 2 },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 4, suit: 'clubs', rank: 4 },
+                { value: 4, suit: 'spaces', rank: 4 },
             ];
     
             const result = getHandName(fullHouseHand);
@@ -94,10 +94,10 @@ describe('Hand calculator test suite', () => {
 
     it('should return Four of a Kind when the only applicable card is that', () => {
             const fourOfAKindHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'diamonds' },
-                { value: 2, suit: 'hearts' },
-                { value: 2, suit: 'clubs' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'diamonds', rank: 2 },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 2, suit: 'clubs', rank: 2 },
             ];
     
             const result = getHandName(fourOfAKindHand);
@@ -106,11 +106,11 @@ describe('Hand calculator test suite', () => {
 
     it('should return Straight Flush when the only applicable card is that', () => {
             const straightFlushHand = [
-                { value: 2, suit: 'hearts' },
-                { value: 3, suit: 'hearts' },
-                { value: 4, suit: 'hearts' },
-                { value: 5, suit: 'hearts' },
-                { value: 6, suit: 'hearts' },
+                { value: 2, suit: 'hearts', rank: 2 },
+                { value: 3, suit: 'hearts', rank: 3 },
+                { value: 4, suit: 'hearts', rank: 4 },
+                { value: 5, suit: 'hearts', rank: 5 },
+                { value: 6, suit: 'hearts', rank: 6 },
             ];
     
             const result = getHandName(straightFlushHand);
--- resources/js/composables/hand-calculator.js
+++ resources/js/composables/hand-calculator.js
@@ -5,39 +5,195 @@ export function useHandCalculator() {
 
     // All of the functions below should return a boolean value, true if the hand is that type, false otherwise.
 
-    // Straight flush: Five cards in a sequence, all in the same suit.
-    const isStraightFlush = (cards) => {}
+    const sortCards = (cards) => cards.sort((a, b) => a.rank - b.rank);
 
-    // Four of a kind: Four cards of the same rank, and suit
-    const isFourOfAKind = (cards) => {}
+    // Straight flush: Five cards in a sequence, all in the same suit.
+    const isStraightFlush = (cards) => {
+        if (!cards.every((card) => card.suit === cards[0].suit)) {
+            return false;
+        }
+        sortCards(cards);
+
+        //Checks for low ace
+        if (
+            cards.length === 5 &&
+            cards[4].rank === 14 &&
+            cards[0].rank === 2 &&
+            cards[1].rank === 3 &&
+            cards[2].rank === 4 &&
+            cards[3].rank === 5
+        ) {
+            return true;
+        }
+
+        return cards.every((card, index) => {
+            if (index > 0) {
+                return card.rank === cards[index - 1].rank + 1;
+            }
+            return true;
+        });
+    };
+
+    // Four of a kind: Four cards of the same rank
+    const isFourOfAKind = (cards) => {
+        const rankCounts = {};
+        for (const card of cards) {
+            if (rankCounts[card.rank]) {
+                rankCounts[card.rank] += 1;
+            } else {
+                rankCounts[card.rank] = 1;
+            }
+        }
+        for (const rank in rankCounts) {
+            if (rankCounts[rank] === 4) {
+                return true;
+            }
+        }
+        return false;
+    };
 
     // Full house: Three cards of one rank and two cards of another rank.
-    const isFullHouse = (cards) => {}
+    const isFullHouse = (cards) => {
+        const rankCounts = {};
+        for (const card of cards) {
+            if (rankCounts[card.rank]) {
+                rankCounts[card.rank] += 1;
+            } else {
+                rankCounts[card.rank] = 1;
+            }
+        }
+        let hasPair = false;
+        let hasThreeOfAKind = false;
+        let fullHouse = false;
+        for (const key in rankCounts) {
+            if (!fullHouse) {
+                if (rankCounts[key] === 2) {
+                    hasPair = true;
+                }
+                if (rankCounts[key] === 3) {
+                    hasThreeOfAKind = true;
+                }
+                if (hasPair && hasThreeOfAKind) {
+                    fullHouse = true;
+                }
+            }
+        }
+        return fullHouse;
+    };
 
     // Flush: Five cards of the same suit, not in sequence
-    const isFlush = (cards) => {}
+    const isFlush = (cards) =>
+        cards.every((card) => card.suit === cards[0].suit);
 
     // Straight: Five cards in a sequence, but not of the same suit.
-    const isStraight = (cards) => {}
+    const isStraight = (cards) => {
+        sortCards(cards);
+
+        //Checks for low ace
+        if (
+            cards.length === 5 &&
+            cards[4].rank === 14 &&
+            cards[0].rank === 2 &&
+            cards[1].rank === 3 &&
+            cards[2].rank === 4 &&
+            cards[3].rank === 5
+        ) {
+            return true;
+        }
+
+        for (let i = 1; i < cards.length; i++) {
+            if (cards[i].rank !== cards[i - 1].rank + 1) {
+                return false;
+            }
+        }
+        return true;
+    };
 
     // Three of a kind: Three cards of the same rank
-    const isThreeOfAKind = (cards) => {}
+    const isThreeOfAKind = (cards) => {
+        const rankCounts = {};
+        for (const card of cards) {
+            if (rankCounts[card.rank]) {
+                rankCounts[card.rank] += 1;
+            } else {
+                rankCounts[card.rank] = 1;
+            }
+        }
+        for (const rank in rankCounts) {
+            if (rankCounts[rank] === 3) {
+                return true;
+            }
+        }
+        return false;
+    };
 
     // Two pair: Two cards of one rank and two cards of another rank
-    const isTwoPair = (cards) => {}
+    const isTwoPair = (cards) => {
+        const rankCounts = {};
+        for (const card of cards) {
+            if (rankCounts[card.rank]) {
+                rankCounts[card.rank] += 1;
+            } else {
+                rankCounts[card.rank] = 1;
+            }
+        }
+
+        let pairs = 0;
+        for (const key in rankCounts) {
+            if (rankCounts[key] === 2) {
+                pairs++;
+
+                if (pairs === 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
 
     // Pair: Two cards of the same rank
-    const isPair = (cards) => {}
+    const isPair = (cards) => {
+        const ranks = [];
+        for (const card of cards) {
+            if (ranks.includes(card.rank)) {
+                return true;
+            } else {
+                ranks.push(card.rank);
+            }
+        }
+        return false;
+    };
 
     // High card: Highest value card (last ditch saloon, if none of those above apply, this one will)
-    const isHighCard = (cards) => {}
-
+    const isHighCard = (cards) => {
+        sortCards(cards);
+        console.log(cards[0]);
+        return true;
+    };
 
     const getHandName = (cards) => {
-        // logic!
-    }
+        if (isStraightFlush(cards)) {
+            return "Straight Flush";
+        } else if (isFourOfAKind(cards)) {
+            return "Four of a Kind";
+        } else if (isFullHouse(cards)) {
+            return "Full House";
+        } else if (isFlush(cards)) {
+            return "Flush";
+        } else if (isStraight(cards)) {
+            return "Straight";
+        } else if (isThreeOfAKind(cards)) {
+            return "Three of a Kind";
+        } else if (isTwoPair(cards)) {
+            return "Two Pair";
+        } else if (isPair(cards)) {
+            return "Pair";
+        }
+
+        return "High Card";
+    };
 
     return {
-        getHandName
-    }
+        getHandName,
+    };
 }
