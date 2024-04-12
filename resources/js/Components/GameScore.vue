84d1783ff193e26d37a625415ca446a470c5bed6
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2024-04-12 13:42:50 +0100

    added GameScore component

---
+++ resources/js/Components/GameScore.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+
+defineProps(["totalScore", "multiplier"]);
+</script>
+
+<template>
+    <div class="mb-20 flex gap-5">
+        <div
+            class="flex justify-center w-24 mt-10 bg-indigo-600 text-white border rounded p-3"
+        >
+            {{ totalScore }}
+        </div>
+        <FontAwesomeIcon :icon="faX" class="text-white self-center mt-10" />
+        <div
+            class="flex justify-center w-24 mt-10 bg-red-600 text-white border rounded p-3"
+        >
+            {{ multiplier }}
+        </div>
+    </div>
+</template>
