642ce6f8d2b90dcf5ae287543566f627e75d2ce2
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2024-04-12 15:52:24 +0100

    created games db

---
+++ app/Models/Game.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    protected $table = 'games';
+
+    protected $fillable = [
+        'id',
+        'round',
+        'status',
+    ];
+}
