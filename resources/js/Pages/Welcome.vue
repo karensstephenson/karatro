3e7aa9a4670d6f795ce78fa2ea1839ed07cc688d
karensstephenson <135253615+karensstephenson@users.noreply.github.com>
2024-04-13 14:08:41 +0100

    added Welcome page with link to Game.vue (#79)
    * added Welcome page with link to Game.vue
    
    * removed props

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Karatro" />
+    <div
+        class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 green-bg"
+    >
+        <div
+            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
+        >
+            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                <main class="mt-6 flex flex-col items-center justify-center">
+                    <h1 class="text-white text-6xl">Karatro</h1>
+
+                    <div class="flex flex-col items-center">
+                        <div class="flex gap-3">
+                            <Link
+                                :href="route('game')"
+                                class="mt-6 px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
+                                >New Game</Link
+                            >
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.green-bg {
+    background-color: #00796b;
+}
+</style>
