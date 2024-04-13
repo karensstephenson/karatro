1886091e259bd83c0bc757a7633c51bde235d68e
James <electionnightspecial@gmail.com>
2024-04-13 21:11:45 +0100

    Minor tweaks to get it working

--- database/migrations/2024_04_12_142432_create-games-table.php
+++ database/migrations/2024_04_12_142432_create-games-table.php
@@ -4,18 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('games', function (Blueprint $table) {
-            $table->bigInteger('id');
-            $table->integer('round');
-            $table->integer('hands');
-            $table->integer('discards');
+            $table->id();
+            $table->integer('round')->default(1);
+            $table->integer('hands')->default(4);
+            $table->integer('discards')->default(3);
             $table->timestamps();
         });
     }
