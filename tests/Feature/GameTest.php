a916b4724f61c7b10ce13b7eecd3f05af8259529
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2024-04-15 10:50:04 +0100

    added tests for creation of new game and redirect to new game id

---
+++ tests/Feature/GameTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Game;
+
+use Tests\TestCase;
+
+use App\Models\Game;
+use Inertia\Testing\AssertableInertia;
+
+class GameTest extends TestCase
+{
+    public function testNewGameCreated(): void
+    {
+        $initialGameCount = Game::count();
+
+        $response = $this->post('/game');
+
+        $this->assertEquals($initialGameCount + 1, Game::count());
+
+        $game = Game::latest()->first();
+
+        $response->assertRedirect(route('game', ['gameId' => $game->id]));
+    }
+
+    public function testEndUpOnGameId(): void
+    {
+        $response = $this->get('/game/{gameId');
+
+        $response->assertStatus(200);
+
+        $response->assertInertia(
+            fn (AssertableInertia $page) => $page
+                ->component('Game')
+                ->has('cardList')
+        );
+    }
+}
