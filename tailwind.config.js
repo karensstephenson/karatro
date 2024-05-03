a903dadfc28ff9164c732481b3c018ccb91d1f68
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2024-05-03 18:33:07 +0100

    added pop animation when scoring cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,38 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
             gridTemplateColumns: {
-                '13': 'repeat(13, minmax(0, 1fr))'
+                13: "repeat(13, minmax(0, 1fr))",
+            },
+            keyframes: {
+                pop: {
+                    "0%": {
+                        transform: "scale(1)",
+                    },
+                    "50%": {
+                        transform: "scale(1.2)",
+                    },
+                    "100%": {
+                        transform: "scale(1)",
+                    },
+                },
+            },
+            animation: {
+                'pop': 'pop 1.5s'
             }
         },
     },
