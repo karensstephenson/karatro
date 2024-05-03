c25aba337b9194c152b517b84820d421165ea4bc
karensstephenson <135253615+karensstephenson@users.noreply.github.com>
2024-05-03 09:00:24 +0100

    149 show remaining cards (#163)
    * added deck UI
    
    * reset played cards and discards

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            gridTemplateColumns: {
+                '13': 'repeat(13, minmax(0, 1fr))'
+            }
         },
     },
 
