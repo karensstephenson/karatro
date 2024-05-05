83fe4296845c5243a2596d99cd9f864e81406d08
karensstephenson <135253615+karensstephenson@users.noreply.github.com>
2024-05-05 10:36:22 +0100

    Added NewRound component (#175)
    * Added NewRound component
    
    * removed unused imports
    
    * removed script tag

---
+++ resources/js/Components/NewRound.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="flex justify-center border-blue-800 rounded border-8 bg-black m-2"
+    >
+        <div class="m-2">
+            <button
+                class="justify-center px-4 py-1 text-white bg-orange-600 rounded-md hover:bg-orange-700 w-full"
+            >
+                Select
+            </button>
+            <p
+                class="mt-4 border rounded-full text-white bg-blue-900 flex justify-center w-full"
+            >
+                Small Blind
+            </p>
+            <p
+                class="mt-4 mx-auto border text-white bg-blue-900 w-20 h-20 rounded-full flex justify-center items-center text-center"
+            >
+                SMALL BLIND
+            </p>
+            <div
+                class="mt-4 px-4 w-full py-2 border rounded text-white text-center"
+            >
+                <p>Score at least</p>
+                <p class="text-red-600 font-bold items-center">300</p>
+                <p>Reward: <span class="text-orange-600">$$$+</span></p>
+            </div>
+            <p class="text-white flex justify-center pt-2">or</p>
+            <div class="flex justify-content gap-2">
+                <p class="w-10 border rounded mt-2"></p>
+                <button
+                    class="mt-2 px-4 py-1 text-white bg-red-600 rounded-md hover:bg-red-700 w-full"
+                >
+                    Skip Blind
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
