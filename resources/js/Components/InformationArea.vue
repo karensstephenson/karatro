ff5af33ef63863a4e1c3e37c44bc3e33ec137a31
karensstephenson <135253615+karensstephenson@users.noreply.github.com>
2024-05-09 12:23:35 +0100

    176 refactor components (#177)
    * added GameArea component
    
    * added information area component
    
    * formatting

---
+++ resources/js/Components/InformationArea.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { useGameStore } from "@/stores/game";
+import { defineProps } from "vue";
+import GameScore from "@/Components/GameScore.vue";
+
+const gameStore = useGameStore();
+
+const props = defineProps<{
+    cash: number;
+}>();
+</script>
+
+<template>
+    <div class="mt-6 text-sm h-full">
+        <div
+            class="flex items-center w-28 mt-5 bg-gray-600 text-white border rounded p-3 mb-2 w-full justify-around"
+        >
+            <p>Target Score</p>
+            <p>{{ gameStore.targetScore }}</p>
+        </div>
+        <div
+            class="flex items-center w-28 mt-5 bg-gray-600 text-white border rounded p-3 mb-2 w-full justify-around"
+        >
+            <p>Round Score</p>
+            <p>{{ gameStore.roundPoints }}</p>
+        </div>
+        <GameScore
+            :chips="gameStore.chips"
+            :multiplier="gameStore.multiplier"
+            :showPlayerHand="gameStore.showPlayerHand"
+        />
+
+        <div class="grid grid-cols-3 grid-rows-6 gap-1">
+            <div
+                class="flex flex-col row-start-1 row-span-3 items-center h-full bg-gray-600 text-white border rounded p-3"
+            >
+                <p>Info</p>
+            </div>
+
+            <div
+                class="flex flex-col items-center bg-gray-600 text-white border rounded p-3 col-start-2 row-span-2"
+            >
+                <p>Hands</p>
+                <p>{{ gameStore.remainingHands }}</p>
+            </div>
+
+            <div
+                class="flex flex-col items-center bg-gray-600 text-white border rounded p-3 col-start-3 row-span-2"
+            >
+                <p>Discards</p>
+                <p>{{ gameStore.remainingDiscards }}</p>
+            </div>
+
+            <div
+                class="flex flex-col row-start-4 row-span-3 items-center h-full bg-gray-600 text-white border rounded p-3"
+            >
+                <p>Options</p>
+            </div>
+            <div
+                class="flex flex-col items-center justify-center bg-gray-600 text-white border rounded p-3 col-start-2 row-start-3 row-span-2 col-span-2 text-xl"
+            >
+                <p>$ {{ cash }}</p>
+            </div>
+            <div
+                class="flex flex-col row-start-5 row-span-2 items-center w-18 h-full bg-gray-600 text-white border rounded p-3"
+            >
+                Ante
+            </div>
+            <div
+                class="flex flex-col row-start-5 row-span-2 items-center w-18 h-full bg-gray-600 text-white border rounded p-3"
+            >
+                Round
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Game.vue
+++ resources/js/Pages/Game.vue
@@ -2,19 +2,15 @@
 import { defineProps, ref, computed, onMounted } from "vue";
 import { Head, Link } from "@inertiajs/vue3";
 import cards from "@/global/cards";
-import draggable from "vuedraggable";
 import { router } from "@inertiajs/vue3";
 
 import { useGameStore } from "@/stores/game";
 
-import HandSort from "@/Components/HandSort.vue";
-import GameScore from "@/Components/GameScore.vue";
-import HandsAndDiscards from "@/Components/HandsAndDiscards.vue";
-import CashAndTotalPoints from "@/Components/CashAndTotalPoints.vue";
-import PlayedCards from "@/Components/PlayedCards.vue";
 import GameStatus from "@/Components/GameStatus.vue";
 import Deck from "@/Components/Deck.vue";
 import NewRound from "@/Components/NewRound.vue";
+import GameArea from "@/Components/GameArea.vue";
+import InformationArea from "@/Components/InformationArea.vue";
 
 const gameStore = useGameStore();
 
@@ -37,14 +33,7 @@ const props = defineProps<{
 
 const winGame = computed(() => gameStore.roundPoints >= gameStore.targetScore);
 
-const isButtonDisabled = computed(() => gameStore.selectedCards.length === 0);
-
 let isGameOver = computed(() => gameStore.remainingHands <= 0);
-
-let isDiscardsZero = computed(
-    () =>
-        gameStore.remainingDiscards <= 0 || gameStore.selectedCards.length === 0
-);
 
 const newGame = () => {
     router.get(route("home"));
@@ -156,159 +145,16 @@ const toggleCardDeck = () => {
                     class="grid grid-cols-3 relative mt-6 flex items-center gap-3"
                 >
                     <!-- SCORING SECTION -->
-                    <div class="mt-6 text-sm h-full">
-                        <div
-                            class="flex items-center w-28 mt-5 bg-gray-600 text-white border rounded p-3 mb-2 w-full justify-around"
-                        >
-                            <p>Target Score</p>
-                            <p>{{ gameStore.targetScore }}</p>
-                        </div>
-                        <div
-                            class="flex items-center w-28 mt-5 bg-gray-600 text-white border rounded p-3 mb-2 w-full justify-around"
-                        >
-                            <p>Round Score</p>
-                            <p>{{ gameStore.roundPoints }}</p>
-                        </div>
-                        <GameScore
-                            :chips="gameStore.chips"
-                            :multiplier="gameStore.multiplier"
-                            :showPlayerHand="gameStore.showPlayerHand"
-                        />
-
-                        <div class="grid grid-cols-3 grid-rows-6 gap-1">
-                            <div
-                                class="flex flex-col row-start-1 row-span-3 items-center h-full bg-gray-600 text-white border rounded p-3"
-                            >
-                                <p>Info</p>
-                            </div>
-
-                            <div
-                                class="flex flex-col items-center bg-gray-600 text-white border rounded p-3 col-start-2 row-span-2"
-                            >
-                                <p>Hands</p>
-                                <p>{{ gameStore.remainingHands }}</p>
-                            </div>
-
-                            <div
-                                class="flex flex-col items-center bg-gray-600 text-white border rounded p-3 col-start-3 row-span-2"
-                            >
-                                <p>Discards</p>
-                                <p>{{ gameStore.remainingDiscards }}</p>
-                            </div>
-
-                            <div
-                                class="flex flex-col row-start-4 row-span-3 items-center h-full bg-gray-600 text-white border rounded p-3"
-                            >
-                                <p>Options</p>
-                            </div>
-                            <div
-                                class="flex flex-col items-center justify-center bg-gray-600 text-white border rounded p-3 col-start-2 row-start-3 row-span-2 col-span-2 text-xl"
-                            >
-                                <p>$ {{ cash }}</p>
-                            </div>
-                            <!-- <div
-                                class="col-start-2 row-start-3 row-span-2 h-full"
-                            >
-                                <CashAndTotalPoints
-                                    :cash="cash"
-                                    :totalPoints="gameStore.totalPoints"
-                                />
-                            </div> -->
-                            <div
-                                class="flex flex-col row-start-5 row-span-2 items-center w-18 h-full bg-gray-600 text-white border rounded p-3"
-                            >
-                                Ante
-                            </div>
-                            <div
-                                class="flex flex-col row-start-5 row-span-2 items-center w-18 h-full bg-gray-600 text-white border rounded p-3"
-                            >
-                                Round
-                            </div>
-                        </div>
-                    </div>
+                    <InformationArea :cash="cash" />
 
                     <!-- CARD SECTION -->
-                    <div
-                        class="col-start-2 col-span-2 mt-6 flex flex-col items-center w-full"
-                    >
-                        <div class="h-40 flex flex-col">
-                            <PlayedCards
-                                class="flex flex-col h-24"
-                                v-if="gameStore.isPlayHandClicked"
-                            />
-                        </div>
+                    <GameArea
+                        @playHand="playHand()"
+                        @updateDiscardCards="updateDiscardCards()"
+                        @toggleCardDeck="toggleCardDeck()"
+                    />
 
-                        <div class="flex flex-col items-center">
-                            <draggable
-                                class="flex justify-center gap-3 list-none"
-                                v-model="gameStore.hand"
-                                :item-key="(item) => item.id"
-                            >
-                                <template #item="{ element }">
-                                    <div>
-                                        <li
-                                            :key="element"
-                                            class="card"
-                                            :class="{
-                                                selected:
-                                                    gameStore.isSelected(
-                                                        element
-                                                    ),
-                                            }"
-                                            @click="
-                                                gameStore.toggleCard(element)
-                                            "
-                                        >
-                                            <img
-                                                :src="`../card_svgs/${element.name}.svg`"
-                                                :alt="element.name"
-                                                class="h-24"
-                                            />
-                                        </li>
-                                    </div>
-                                </template>
-                            </draggable>
-                        </div>
-                        <div class="flex gap-3">
-                            <button
-                                :disabled="isButtonDisabled"
-                                :class="{
-                                    'opacity-50 cursor-not-allowed':
-                                        isButtonDisabled,
-                                }"
-                                class="mt-6 px-4 py-6 w-20 text-white bg-indigo-600 rounded-md border hover:bg-indigo-700"
-                                @click="playHand"
-                            >
-                                Play Hand
-                            </button>
-
-                            <HandSort
-                                @sortByRank="gameStore.sortByRank"
-                                @sortBySuit="gameStore.sortBySuit"
-                                class="self-center"
-                            />
-
-                            <button
-                                :disabled="isDiscardsZero"
-                                :class="{
-                                    'opacity-50 cursor-not-allowed':
-                                        isDiscardsZero,
-                                }"
-                                class="mt-6 px-4 py-6 w-20 text-white bg-red-600 rounded-md border hover:bg-red-700"
-                                @click="updateDiscardCards"
-                            >
-                                Discard
-                            </button>
-                            <button
-                                class="self-end h-10 w-10 text-white text-xs bg-white-600 rounded-md border hover:bg-neutral-500"
-                                @click="toggleCardDeck"
-                            >
-                                Deck
-                            </button>
-                        </div>
-                    </div>
-
-                    <!-- GAME OVER -->
+                    <!-- END OF GAME -->
                     <div
                         v-if="isGameOver"
                         class="col-start-1 col-end-4 row-start-1 row-end-4 flex items-center justify-center absolute inset-0 bg-black bg-opacity-75"
